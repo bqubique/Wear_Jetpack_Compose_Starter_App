0290466e20af55a02f0c915f6bdac5b29a8276f3
bqubique <blinnqipa@gmail.com>
2022-04-17 21:48:31 +0200

    Change the base to official wear compose starter. Update libraries.

--- app/src/main/java/com/bqubique/wearjetpackcompose/WearApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.bqubique.wearjetpackcompose
-
-import android.app.Application
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class WearApplication: Application()
--- app/src/main/java/com/bqubique/wearjetpackcompose/api/Api.kt
+++
@@ -1,6 +0,0 @@
-package com.bqubique.wearjetpackcompose.api
-
-interface Api {
-//    @GET("api/v4/verses/random?language=en&translations=22#")
-//    suspend fun getRandomAyah(): Response<Ayah>
-}
